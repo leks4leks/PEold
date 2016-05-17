c9c14d978f20458af02c89c234c4a16794d91cf6
DmitriyLer <dl_demon@mail.ru>
2016-05-17 20:13:19 +0600

    Депозитный калькулятор логика

--- ProgressoExpert/ProgressoExpert/Controls/Calculators/DepositCalculatorWindow.xaml.cs
+++ ProgressoExpert/ProgressoExpert/Controls/Calculators/DepositCalculatorWindow.xaml.cs
@@ -19,10 +19,94 @@ namespace ProgressoExpert.Controls.Calculators
     /// </summary>
     public partial class DepositCalculatorWindow : Window
     {
-        
+        const int daysInMonth = 30;
+
+        private double _sum = 0;
+        public double Sum
+        {
+            get { return _sum; }
+            set
+            {
+                if (value != _sum)
+                {
+                    _sum = value;
+                }
+            }
+        }
+        private double _annualRate = 0;
+        public double AnnualRate
+        {
+            get { return _annualRate; }
+            set
+            {
+                if (value != _annualRate)
+                {
+                    _annualRate = value;
+                }
+            }
+        }
+        int months;
+        int daysInYear;
+
+        double percentInMonth;
+        double incomeTax;
+        double profit;
+
+        private double _sumTotal = 0;
+        public double SumTotal
+        {
+            get { return _sumTotal; }
+            set
+            {
+                if (value != _sumTotal)
+                {
+                    _sumTotal = value;
+                }
+            }
+        }
+        private double _percentTotal = 0;
+        public double PercentTotal
+        {
+            get { return _percentTotal; }
+            set
+            {
+                if (value != _percentTotal)
+                {
+                    _percentTotal = value;
+                }
+            }
+        }
+
+        private double _percentForJuric = 20;
+        public double PercentForJuric
+        {
+            get { return _percentForJuric; }
+            set
+            {
+                if (value != _percentForJuric)
+                {
+                    _percentForJuric = value;
+                }
+            }
+        }
+
+        private double _percentForPhys = 10;
+        public double PercentForPhys
+        {
+            get { return _percentForPhys; }
+            set
+            {
+                if (value != _percentForPhys)
+                {
+                    _percentForPhys = value;
+                }
+            }
+        }
+
         public DepositCalculatorWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
         }
 
         private void Window_MouseDown_1(object sender, MouseButtonEventArgs e)
@@ -38,6 +122,20 @@ namespace ProgressoExpert.Controls.Calculators
 
         private void CalcBtn_Click(object sender, RoutedEventArgs e)
         {
+            //sum = Convert.ToDouble(SumTb.Text);
+            //annualRate = Convert.ToDouble(AnnualRateTB.Text);
+            months = Convert.ToInt32(MonthsTb.Text);
+            daysInYear = Convert.ToInt32(DaysInYearTb.Text);
+
+            percentInMonth = Math.Round(Sum * AnnualRate / 100 / daysInYear * daysInMonth, 0);
+            incomeTax = Math.Round(phys.IsSelected ? percentInMonth * PercentForPhys / 100 : percentInMonth * PercentForJuric / 100, 0);
+            profit = Math.Round(percentInMonth - incomeTax, 0);
+
+            SumTotal = profit * months;
+            PercentTotal = Math.Round(SumTotal / Sum * 100, 0);
+
+            //ResultSumTb.Text = sumTotal.ToString();
+            //ResultPercentTb.Text = percentTotal.ToString();
 
         }
     }
